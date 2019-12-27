77d390fa4d65a5598ddfa69ee9edc2adb5ccb030
edonv <22782929+edonv@users.noreply.github.com>
2019-12-26 19:21:16 -0500

    Icons :(

--- js/player.js
+++ js/player.js
@@ -50,25 +50,11 @@ sb_template.innerHTML = `
 
 const svg_template = document.createElement('template');
 svg_template.innerHTML = `
-
-            <symbol id="pause">
-                <path d="M25,0 C11.1928806,0 0,11.1928806 0,25 C0,38.8071194 11.1928806,50 25,50 C38.8071194,50 50,38.8071194 50,25 C50,11.1928806 38.8071194,0 25,0 Z M25,3 C12.8497349,3 3,12.8497349 3,25 C3,37.1502651 12.8497349,47 25,47 C37.1502651,47 47,37.1502651 47,25 C47,12.8497349 37.1502651,3 25,3 Z" fill-rule="evenodd" stroke-width="0"></path>
-                <path d="M16.34,37l0,-24l5,0l0,24zM33.66,37l0,-24l-5,0l0,24z" stroke-linejoin="round"></path>
-            </symbol>
-            <symbol id="play">
-                <path d="M25,0 C11.1928806,0 0,11.1928806 0,25 C0,38.8071194 11.1928806,50 25,50 C38.8071194,50 50,38.8071194 50,25 C50,11.1928806 38.8071194,0 25,0 Z M25,3 C12.8497349,3 3,12.8497349 3,25 C3,37.1502651 12.8497349,47 25,47 C37.1502651,47 47,37.1502651 47,25 C47,12.8497349 37.1502651,3 25,3 Z" fill-rule="evenodd" stroke-width="0"></path>
-                <path d="M37,25L16.34,37L16.34,13z" stroke-linejoin="round"></path>
-            </symbol>
-
-`;
-
-const symbols_template = document.createElement('template');
-symbols_template.innerHTML = `
-    <symbol id="pause" viewBox="0 0 50 50" height="50" width="50">
+    <symbol id="pause">
         <path d="M25,0 C11.1928806,0 0,11.1928806 0,25 C0,38.8071194 11.1928806,50 25,50 C38.8071194,50 50,38.8071194 50,25 C50,11.1928806 38.8071194,0 25,0 Z M25,3 C12.8497349,3 3,12.8497349 3,25 C3,37.1502651 12.8497349,47 25,47 C37.1502651,47 47,37.1502651 47,25 C47,12.8497349 37.1502651,3 25,3 Z" fill-rule="evenodd" stroke-width="0"></path>
         <path d="M16.34,37l0,-24l5,0l0,24zM33.66,37l0,-24l-5,0l0,24z" stroke-linejoin="round"></path>
     </symbol>
-    <symbol id="play" viewBox="0 0 50 50" height="50" width="50">
+    <symbol id="play">
         <path d="M25,0 C11.1928806,0 0,11.1928806 0,25 C0,38.8071194 11.1928806,50 25,50 C38.8071194,50 50,38.8071194 50,25 C50,11.1928806 38.8071194,0 25,0 Z M25,3 C12.8497349,3 3,12.8497349 3,25 C3,37.1502651 12.8497349,47 25,47 C37.1502651,47 47,37.1502651 47,25 C47,12.8497349 37.1502651,3 25,3 Z" fill-rule="evenodd" stroke-width="0"></path>
         <path d="M37,25L16.34,37L16.34,13z" stroke-linejoin="round"></path>
     </symbol>
@@ -140,7 +126,7 @@ class Songbox extends HTMLElement {
 //        playIconUse.setAttribute('y', "0");
 //        playIconUse.setAttribute('height', "34");
 //        playIconUse.setAttribute('width', "34");
-//        playIconSVG.appendChild(svg_template.content.cloneNode(true));
+        playIconSVG.appendChild(svg_template.content.cloneNode(true));
         playIconSVG.appendChild(playIconUse);
         playbtn.appendChild(playIconSVG);
         shadow.appendChild(playbtn);
